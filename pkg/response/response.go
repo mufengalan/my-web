19e521f458c6ec8793a4ea695ede79cde1404c84
jcy <jichunyu@makeblock.com>
2025-03-31 18:16:00 +0800

    feat: enhance MySQL configuration with connection pooling and retry logic

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,25 +1,68 @@
 package response
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Response struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
-	Data interface{} `json:"data"`
+	Data interface{} `json:"data,omitempty"`
 }
 
-func Success(c *gin.Context, data interface{}) {
-	c.JSON(200, Response{
-		Code: 0,
-		Msg:  "success",
+const (
+	SuccessCode       = 0
+	InvalidParamsCode = 10001
+	UnauthorizedCode  = 10002
+	InternalErrorCode = 10003
+)
+
+func Result(c *gin.Context, httpStatus int, code int, msg string, data interface{}) {
+	c.JSON(httpStatus, Response{
+		Code: code,
+		Msg:  msg,
 		Data: data,
 	})
 }
 
-func Err(c *gin.Context, code int, msg string) {
-	c.JSON(200, Response{
-		Code: code,
-		Msg:  msg,
-		Data: nil,
-	})
+func Success(c *gin.Context, data interface{}) {
+	Result(c, http.StatusOK, SuccessCode, "success", data)
+}
+
+func SuccessWithMessage(c *gin.Context, message string, data interface{}) {
+	Result(c, http.StatusOK, SuccessCode, message, data)
+}
+
+func Created(c *gin.Context, data interface{}) {
+	Result(c, http.StatusCreated, SuccessCode, "created", data)
+}
+
+func Updated(c *gin.Context, data interface{}) {
+	Result(c, http.StatusOK, SuccessCode, "updated", data)
+}
+
+func Deleted(c *gin.Context) {
+	Result(c, http.StatusNoContent, SuccessCode, "deleted", nil)
+}
+
+func Err(c *gin.Context, httpStatus int, code int, msg string) {
+	Result(c, httpStatus, code, msg, nil)
+}
+
+func ErrInvalidParams(c *gin.Context) {
+	Err(c, http.StatusBadRequest, InvalidParamsCode, "invalid parameters")
+}
+
+func ErrInternal(c *gin.Context) {
+	Err(c, http.StatusInternalServerError, InternalErrorCode, "internal server error")
+}
+
+func ErrUnauthorized(c *gin.Context) {
+	Err(c, http.StatusUnauthorized, UnauthorizedCode, "unauthorized")
+}
+
+func ErrWithData(c *gin.Context, httpStatus int, code int, msg string, data interface{}) {
+	Result(c, httpStatus, code, msg, data)
 }
