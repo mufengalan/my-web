e217c7cd62b78875eae4a3cc77115f669c1712f3
jcy <jichunyu@makeblock.com>
2025-03-31 18:22:28 +0800

    fix: update error handling in example and export handlers to use HTTP status codes

--- internal/handler/example.go
+++ internal/handler/example.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mufengalan/my-web/internal/service"
 	"github.com/mufengalan/my-web/pkg/response"
@@ -10,7 +12,7 @@ func GetList(c *gin.Context) {
 	src := service.NewExampleService()
 	list, err := src.GetExampleList()
 	if err != nil {
-		response.Err(c, -101, err.Error())
+		response.Err(c, http.StatusInternalServerError, 1000, err.Error())
 		return
 	}
 	response.Success(c, list)
--- internal/handler/export.go
+++ internal/handler/export.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mufengalan/my-web/internal/service"
 	"github.com/mufengalan/my-web/pkg/response"
@@ -15,15 +17,15 @@ func (h *ExportHandler) ExportData(c *gin.Context) {
 	//从headers获取token
 	token := c.GetHeader("token")
 	url := c.GetHeader("url")
-	http := utils.NewHttp(token)
-	resp, err := http.GetRequest(url)
+	httpClient := utils.NewHttp(token)
+	resp, err := httpClient.GetRequest(url)
 	if err != nil {
-		response.Err(c, -100, "请求失败")
+		response.Err(c, http.StatusInternalServerError, 1000, err.Error())
 		return
 	}
 	err = service.NewExportService().Export(resp)
 	if err != nil {
-		response.Err(c, -100, "导出失败")
+		response.Err(c, http.StatusInternalServerError, 1000, err.Error())
 		return
 	}
 	response.Success(c, nil)
